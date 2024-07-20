4c94df3d4da5c8cba6dc1dcaa22525274ddd966d
Samir <sharipovsamir480@gmail.com>
2024-07-21 02:07:53 +0500

    bismillah

---
+++ src/main/java/sof/uz/outlay/OutlayAppBackendApplication.java
@@ -0,0 +1,13 @@
+package sof.uz.outlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutlayAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutlayAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sof/uz/outlay/OutlayAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sof.uz.outlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutlayAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
