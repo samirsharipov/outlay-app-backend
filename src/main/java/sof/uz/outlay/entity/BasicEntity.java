758b1d6234c10f05aa172f73185f2e8887ae8e9d
acer NITRO 5 <feruzbekhamrayev2002@gmail.com>
2024-08-21 20:39:59 +0500

    user entity added

--- src/main/java/sof/uz/outlay/entity/BasicEntity.java
+++ src/main/java/sof/uz/outlay/entity/BasicEntity.java
@@ -7,11 +7,9 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.yaml.snakeyaml.events.Event;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
-import java.util.Date;
 import java.util.UUID;
 
 @Data
