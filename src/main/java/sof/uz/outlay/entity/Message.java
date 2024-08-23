0ed81751790a3534644a4328ee87553288b80315
Samir <sharipovsamir480@gmail.com>
2024-08-23 16:09:33 +0500

    enitiy done

--- src/main/java/sof/uz/outlay/entity/Message.java
+++ src/main/java/sof/uz/outlay/entity/Message.java
@@ -4,12 +4,13 @@ import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import sof.uz.outlay.entity.template.BasicEntity;
 
 @Entity(name = "message")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Message extends BasicEntity{
+public class Message extends BasicEntity {
     private Integer senderId;
     private Integer qabulId;
 }
